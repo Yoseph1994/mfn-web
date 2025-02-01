2d105d19a3300113b0485e80eada52db54d751bc
unknown <joshrde2002@gmail.com>
2025-02-01 14:11:20 +0300

    fixed issues to start from zero

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,40 +1,7 @@
 import React from "react";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Label } from "@/components/ui/label";
 
-function ForgotPasswordPage() {
-  return (
-    <div className="flex items-center justify-center">
-      <form className="space-y-6 w-full max-w-lg px-20 py-10 bg-white shadow-md rounded-lg">
-        <h2 className="text-2xl font-bold text-center">Forgot Password</h2>
-        <div>
-          <Label
-            htmlFor="email"
-            className="block text-sm font-medium text-gray-700"
-          >
-            Email address
-          </Label>
-          <Input
-            id="email"
-            name="email"
-            type="email"
-            autoComplete="email"
-            required
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
-          />
-        </div>
-        <div>
-          <Button
-            type="submit"
-            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-          >
-            Send Reset Link
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
+function ForgetPassWordPage() {
+  return <div>ForgetPassWordPage</div>;
 }
 
-export default ForgotPasswordPage;
+export default ForgetPassWordPage;
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,7 @@
-import Navbar from "@/components/User/Navbar";
 import React from "react";
 
 function layout({ children }: { children: React.ReactNode }) {
-  return (
-    <div>
-      <Navbar />
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
 
 export default layout;
